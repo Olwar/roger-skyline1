93c0829eeee7dfb300092532267c419e337c1914
Olli Airola <oairola@c1r1p2.hive.fi>
2022-06-09 11:03:16 +0300

    100% done

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo "copying the website..."
-scp -P 50000 index.nginx-debian.html oairola@10.11.247.17:/var/www/html/
+scp -P 50000 index.nginx-debian.html oairola@10.11.248.17:/home/oairola
 sleep 2
 
+ssh -t -p 50000 oairola@10.11.248.17 "sudo mv /home/oairola/index.nginx-debian.html /var/www/html"
+
 echo "restarting the remote server..."
-ssh -t -p 50000 oairola@10.11.247.17 "sudo systemctl restart nginx"
+ssh -t -p 50000 oairola@10.11.248.17 "sudo systemctl restart nginx"
 
 echo "done."
